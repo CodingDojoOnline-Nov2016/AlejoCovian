ba39280c092178df580a180c945a2b67e456fb7d
Alex Covian <alejandrocovian3@gmail.com>
2017-02-13 16:16:27 -0800

    added Sports_ORM part 2

--- Python/Django/Level_2/courses/apps/courses_app/views.py
+++ Python/Django/Level_2/courses/apps/courses_app/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import Course
+from .models import Course, Description
 
 # Create your views here.
 def index(request):
 	context = {
-	'courses' : Course.objects.all()
+	'courses' : Course.objects.all(),
+	'descriptions': Description.objects.all()
 	}
 	return render(request, 'courses_app/index.html', context)
 
 def course(request):
-	if request.POST['name']>0 and request.POST['description']>0:
-		Course.objects.create(name=request.POST['name'], description=request.POST['description'])
+	Course.objects.create(name=request.POST['name'])
+	Description.objects.create(description=request.POST['description'])
 	return redirect('/')
 
 def show(request, id):
