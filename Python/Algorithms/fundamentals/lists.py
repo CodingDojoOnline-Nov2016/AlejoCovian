3e813f08a4e6dbb747334ccf1dcd2f007f6a5b22
Alex Covian <alejandrocovian3@gmail.com>
2018-04-22 17:02:02 -0700

    added strings and lists assignments

---
+++ Python/Algorithms/fundamentals/lists.py
@@ -0,0 +1,11 @@
+drawer = [34, 'envelopes', 'pens']
+print(drawer[0])
+
+x = [1,2,3,4,5]
+x.append(99)
+print(x)
+
+print(max(x))
+
+print(drawer.index(34))
+print (drawer)
---
+++ Python/Algorithms/fundamentals/strings.py
@@ -0,0 +1,6 @@
+first_name = "Alejo"
+last_name = "Covian"
+print ("My name is {} {}".format(first_name, last_name))
+
+x = "Hello World"
+print (x.upper())
