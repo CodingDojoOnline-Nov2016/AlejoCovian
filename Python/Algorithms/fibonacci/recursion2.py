cb0434dd515b70caa14b4a89b7ef7313cd51b075
Alex Covian <alejandrocovian3@gmail.com>
2019-03-11 17:17:57 -0700

    m

--- Python/Algorithms/fibonacci/recursion2.py
+++ Python/Algorithms/fibonacci/recursion2.py
@@ -4,4 +4,4 @@ def fibonacci(n):
 	return fibonacci(n-1)+fibonacci(n-2)
 
 for n in range(1, 8):
-	print n, ":", fibonacci(n)
+	print (n, ":", fibonacci(n))
---
+++ Ruby/examples.rb
@@ -0,0 +1,19 @@
+# def fun x
+#   puts x
+#   puts "Hello World!"
+# end
+#
+# fun "Todd"
+
+# x = [1,2,3,4]
+# for num in (0...4)
+#   puts x[num]
+# end
+
+# arr = ["This", "is", "a", "string"]
+# for i in 0...arr.count
+#   puts arr[i]
+# end
+
+x = puts "hello world"
+puts x
